f7a88bcad89e77c3ded334fd033c5fafdd623ca0
Sümer Cip <sumer.cip@platform.sh>
2024-06-07 16:16:07 +0300

    pass tenant-id from authenticator->resourceattributes processor through the use of from_context

--- custom-otel-collector/myauthext/extension.go
+++ custom-otel-collector/myauthext/extension.go
@@ -25,10 +25,60 @@ func (ba *myAuth) serverStart(_ context.Context, _ component.Host) error {
 	return nil
 }
 
+type myAuthData struct {
+	attributes map[string]any
+}
+
+func NewMyAuthData() *myAuthData {
+	return &myAuthData{
+		attributes: make(map[string]any),
+	}
+}
+
+func (ad *myAuthData) GetAttribute(name string) any {
+	return ad.attributes[name]
+}
+
+func (ad *myAuthData) GetAttributeNames() []string {
+	names := make([]string, 0, len(ad.attributes))
+	for name := range ad.attributes {
+		names = append(names, name)
+	}
+	return names
+}
+
+func (ad *myAuthData) SetAttribute(name string, value any) {
+	ad.attributes[name] = value
+}
+
 func (ba *myAuth) authenticate(ctx context.Context, headers map[string][]string) (context.Context, error) {
-	fmt.Println("authenticate2")
+	fmt.Println("authenticate4")
 	fmt.Println(headers)
-	cl := client.FromContext(ctx)
-	//cl.Auth = authData
-	return client.NewContext(ctx, cl), nil
+	// cl := client.FromContext(ctx)
+	// // //cl.Auth = authData
+	// ctx = client.NewContext(ctx, cl)
+
+	// ctx = context.WithValue(ctx, "auth.tenant_id", "tenant-id-value1")
+	// ctx = context.WithValue(ctx, "tenant_id", "tenant-id-value2")
+
+	// method1 of sharing data with the other components in the pipeline
+	// see: https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/39a7773175cdea009125c59e16acc031eefe2d0c/internal/coreinternal/attraction/attraction.go#L345
+	// function getAttributeValueFromContext
+	cli := client.FromContext(ctx)
+	mdd := map[string][]string{"tenant_id": {"tenant-id-value1"}}
+	md := client.NewMetadata(mdd)
+	cli.Metadata = md
+
+	// method2: setting AuthData
+	ad := NewMyAuthData()
+	ad.SetAttribute("tenant_id", "tenant-id-value2")
+	cli.Auth = ad
+
+	// Set the tenantID in the client
+	//fmt.Println("cli.metadata", cli.Metadata)
+
+	// Put the updated client back into the context
+	ctx = client.NewContext(ctx, cli)
+
+	return ctx, nil
 }
